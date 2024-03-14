99eaba4988875fc1b6b1bd799b3a471166bf7b61
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-14 14:19:21 +0300

    put , delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import { useEffect } from "react";
 
 const BASE_URL = "http://localhost:3005";
 
 function App() {
-  // const getAllUsers = async () => {
-  //   const response = await axios.get(BASE_URL + "/users");
-  //   console.log(response.data);
-  // };
-
-  // const getUserById = async (userId) => {
-  //   const response = await axios.get(BASE_URL + "/users/" + userId);
-  //   console.log(response.data);
-  // };
-  // Veri eklemek için kullanılır
-  const createUser = async (newUser) => {
-    const response = await axios.post(`${BASE_URL}/users`, newUser);
-    console.log("response", response.data);
-  };
-
   useEffect(() => {
-    // getAllUsers();
-    // getUserById(1);
+    const getAllUsers = async () => {
+      const response = await axios.get(BASE_URL + "/users");
+      console.log(response.data);
+    };
+
+    const getUserById = async (userId) => {
+      const response = await axios.get(BASE_URL + "/users/" + userId);
+      console.log(response.data);
+    };
+
+    const createUser = async (newUser) => {
+      const response = await axios.post(`${BASE_URL}/users`, newUser);
+      console.log("response", response.data);
+    };
+
+    const updateUser = async (userId, updatedUser) => {
+      const response = await axios.put(
+        `${BASE_URL}/users/${userId}`,
+        updatedUser
+      );
+      console.log(response.data);
+    };
+
+    const deleteUserById = async (userId) => {
+      const deletedResponse = await axios.delete(`${BASE_URL}/users/${userId}`);
+      console.log(deletedResponse.data);
+    };
+
+    // Kullanımlar
+    getAllUsers();
+    getUserById(1);
     const newUser = {
-      "username ": "malik",
+      username: "malik",
       password: "3434",
     };
     createUser(newUser);
+    updateUser("5", {
+      username: "Alperen",
+      password: "5151",
+    });
+    deleteUserById("1");
   }, []);
 
   return (
